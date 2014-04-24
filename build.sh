4082d9061455d554f17294c9ef771e2eea80d657
root <root@irc.auto.local>
2014-04-24 02:54:27 +0100

    removed cut and paste derp

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 name=charybdis-3.4.2.tar.gz
-ttps://github.com/atheme/charybdis/archive/%{name}-%{version}.tar.gz
 [[ -e "/usr/src/packages/SOURCES/$name" ]] || wget -O /usr/src/packages/SOURCES/$name "https://github.com/atheme/charybdis/archive/$name"
 find SPECS SOURCES -type f -exec /bin/cp -f {} /usr/src/packages/{} \;
 echo "Now run :"
